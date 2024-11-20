50ec87497195323697f73a79fe4dbde2a2b7b212
Kolya <87275865+Kolya142@users.noreply.github.com>
2024-11-21 00:15:03 +0300

    fix installer

--- shell_zavr_pack/zipped.sh
+++ shell_zavr_pack/zipped.sh
@@ -2,4 +2,5 @@ mkdir /tmp/mxgulWGLK4q8U7Ggatap
 echo Y3VybCBodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vS29seWExNDIvc2hlbGwtemF2ci9yZWZzL2hlYWRzL21haW4vc2hlbGxfemF2ci5weSAtbyBzaGVsbF96YXZyLnB5CnN1ZG8gbXYgc2hlbGxfemF2ci5weSAvdXNyL2Jpbi9zaHphdnI= | base64 -d > /tmp/mxgulWGLK4q8U7Ggatap/run.sh
 cd /tmp/mxgulWGLK4q8U7Ggatap
 bash run.sh
+chmod 755 /usr/bin/shzavr
 rm -rdf /tmp/mxgulWGLK4q8U7Ggatap
